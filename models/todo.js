de2702c8e434932379a37b642df76954ec024fe0
JordanTseng <jordan.ttc.design@gmail.com>
2022-04-13 18:41:44 +0800

    測試todo.js拿掉 mongoose.connect()

--- models/todo.js
+++ models/todo.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const DB = process.env.DATABASE.replace(
-  '<password>',
-  process.env.DATABASE_PASSWORD,
-);
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log('success connected to jordan-hex-todo DB');
-  })
-  .catch((err) => console.log(err));
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// dotenv.config({ path: './config.env' });
+// const DB = process.env.DATABASE.replace(
+//   '<password>',
+//   process.env.DATABASE_PASSWORD,
+// );
+// mongoose
+//   .connect(DB)
+//   .then(() => {
+//     console.log('success connected to jordan-hex-todo DB');
+//   })
+//   .catch((err) => console.log(err));
 const todoSchema = new mongoose.Schema(
   {
     content: {
